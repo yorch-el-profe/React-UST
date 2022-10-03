ff0b344ed0bd5c7005a899cdfcc6a6603db35799
Jorge Ramón <jorge.ramon2128@gmail.com>
2022-10-03 17:43:49 -0500

    Solución al Postwork 02

--- react-meals/src/components/app.js
+++ react-meals/src/components/app.js
@@ -1,3 +1,5 @@
+import { useRef, useState } from "react";
+
 function App() {
 	const menu = [
 		{
@@ -17,16 +19,28 @@ function App() {
 		},
 	];
 
+	const [cart, setCart] = useState(0);
+	const inputs = useRef([]);
+
+	function addHandler(index) {
+		const item = menu[index];
+		const ref = inputs.current[index];
+
+		setCart(cart + parseInt(ref.value));
+
+		ref.value = "";
+	}
+
 	return (
 		<>
-			<nav class="navbar navbar-expand-lg navbar-dark bg-danger">
-				<div class="container-fluid">
-					<a class="navbar-brand" href="#">
+			<nav className="navbar navbar-expand-lg navbar-dark bg-danger">
+				<div className="container-fluid">
+					<a className="navbar-brand" href="#">
 						React Meals
 					</a>
-					Carrito <span class="badge bg-danger">2</span>
+					Carrito <span className="badge bg-danger">{cart}</span>
 					<button
-						class="navbar-toggler"
+						className="navbar-toggler"
 						type="button"
 						data-bs-toggle="collapse"
 						data-bs-target="#navbarSupportedContent"
@@ -34,36 +48,46 @@ function App() {
 						aria-expanded="false"
 						aria-label="Toggle navigation"
 					>
-						<span class="navbar-toggler-icon"></span>
+						<span className="navbar-toggler-icon"></span>
 					</button>
-					<div class="collapse navbar-collapse" id="navbarSupportedContent">
-						<ul class="navbar-nav me-auto mb-2 mb-lg-0">
-							<li class="nav-item">
-								<a class="nav-link active" aria-current="page" href="#"></a>
+					<div className="collapse navbar-collapse" id="navbarSupportedContent">
+						<ul className="navbar-nav me-auto mb-2 mb-lg-0">
+							<li className="nav-item">
+								<a className="nav-link active" aria-current="page" href="#"></a>
 							</li>
 						</ul>
 					</div>
 				</div>
 			</nav>
-			<div class="list-group">
-				{menu.map((x) => (
+			<div className="list-group">
+				{menu.map((item, index) => (
 					<a
 						href="#"
-						class="list-group-item list-group-item-action"
+						className="list-group-item list-group-item-action"
 						aria-current="true"
 					>
-						<div class="d-flex w-100 justify-content-between">
-							<h5 class="mb-1">Sushi</h5>
+						<div className="d-flex w-100 justify-content-between">
+							<h5 className="mb-1">{item.name}</h5>
 							<div className="d-flex flex-column">
 								<span>
 									Cantidad
-									<input type="number" />
+									<input
+										type="number"
+										className="form-control"
+										ref={(element) => (inputs.current[index] = element)}
+									/>
 								</span>
-								<button className="btn btn-danger">Agregar</button>
+								<button
+									type="button"
+									className="btn btn-danger"
+									onClick={() => addHandler(index)}
+								>
+									Agregar
+								</button>
 							</div>
 						</div>
-						<p class="mb-1">Preparado con nuestro mejor salmón.</p>
-						<small>$199.00</small>
+						<p className="mb-1">{item.description}</p>
+						<small>${item.price}</small>
 					</a>
 				))}
 			</div>
