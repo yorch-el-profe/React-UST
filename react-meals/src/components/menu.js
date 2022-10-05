29a459d5a2a869fb7aa0530f4c96dd4dce504ad0
Jorge Ramón <jorge.ramon2128@gmail.com>
2022-10-05 01:28:31 -0500

    Terminado rediseño hecho desde cero

--- react-meals/src/components/menu.js
+++ react-meals/src/components/menu.js
@@ -1,11 +1,29 @@
-import MenuItem from "./menu-item";
+import styles from "../styles/menu.module.css";
+import Button from "../ui/button";
 
 function Menu({ items, cart, setCart }) {
 	return (
-		<div className="list-group">
-			{items.map((item) => (
-				<MenuItem item={item} cart={cart} setCart={setCart} />
-			))}
+		<div className={styles["menu-container"]}>
+			<section className={styles.menu}>
+				{items.map((item) => (
+					<article>
+						<div className={styles["menu-item"]}>
+							<div>
+								<h1>{item.name}</h1>
+								<p>{item.description}</p>
+								<small>$ {item.price}</small>
+							</div>
+							<div>
+								<div className={styles["input-container"]}>
+									<span>Cantidad</span>
+									<input type="number" />
+								</div>
+								<Button>+ Agregar</Button>
+							</div>
+						</div>
+					</article>
+				))}
+			</section>
 		</div>
 	);
 }
